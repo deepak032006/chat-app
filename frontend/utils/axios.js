c8f59f0729dfe1d4fbdfd811b26f560ad2483a23
Deeoak sharma <demondark7678@gmail.com>
2025-05-02 18:17:07 +0530

    Update axios.js

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://chat-app-8-8zpp.onrender.com',
     withCredentials:true    
   });
 
