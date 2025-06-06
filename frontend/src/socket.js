506e91d984d9a7dd1b1ee1ec3ec87b8a8e2dd057
Deepak sharma <demondark7678@gmail.com>
2025-06-06 15:54:41 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-app-23-2tzv.onrender.com", {
+const socket = io("https://chat-app-forever-chal.onrender.com", {
   transports: ["websocket"],
   withCredentials: true,
   autoConnect: true,
